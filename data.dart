186df45252a8af19593f0cbbea3a69f819bd4931
ashin000 <ashinpramod010@gmail.com>
2023-10-04 13:23:40 +0530

    Initial commit

---
+++ data.dart
@@ -0,0 +1,24 @@
+void main(){
+  String name ="Ashin";
+  int age = 23;
+  int mob = 9061801150;
+  double mark = 7.1;
+  String email = "ashinpramod001@gmail.com";
+
+  print('My name is $name');
+  print('i am $age yrs old');
+  print('my mobile number is $mob');
+  print('my mark is $mark');
+  print('My email is $email');
+
+
+  var num = 'hello';
+      num ="hello";
+      print(num);
+
+      dynamic x = "hello";
+      x = 200;
+      x = 1.5;
+      x = false;
+      print(x);
+}
